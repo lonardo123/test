c11861d870087bbf40f33418bbf32900f80736f7
lonardo123 <65513096+lonardo123@users.noreply.github.com>
2025-10-04 05:30:38 +0300

    Delete assets/js/core/Start.js

--- assets/js/core/Start.js
+++
@@ -1,4 +0,0 @@
-// مجرد تضمين للتوافق
-window.MainUrl = window.MainUrl || "https://perceptive-victory-production.up.railway.app";
-
-console.log("Start.js loaded - worker starts via Human.js button");
